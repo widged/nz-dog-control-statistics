7c17dbac375e0a4fb892d60028db9ca14ebf5ed0
Marielle Lange <mlange@widged.com>
2016-08-06 13:55:05 +1200

    all undefined

--- react-app/dist/main.1690734f.js
+++ react-app/dist/main.1690734f.js
@@ -23789,9 +23789,9 @@ Whangarei District	2016	OWN	_	0	0	0	0	0	0	0	0	0	8139	79	0	0	0	0	0`
 	    key: 'render',
 	    value: function render() {
 	      var _props = this.props;
-	      var dangerous = _props.dangerous;
-	      var menacing = _props.menacing;
-	      var total = _props.total;
+	      var dangerous = _props.dangerous || {};
+	      var menacing = _props.menacing || {};
+	      var total = _props.total || {};
 
 	      var dangerousClass = !dangerous.all || dangerous.all === 0 ? "dangerous no-data" : "dangerous ";
 	      var menacingClass = !menacing.all || menacing.all === 0 ? "menacing no-data" : "menacing";
